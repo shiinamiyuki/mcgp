cb210e5ac95edd7c754d06b7cdb332497a67dac9
Zewen Shen <zewen.public@gmail.com>
2020-12-01 22:18:55 -0500

    Update test case

--- main.cpp
+++ main.cpp
@@ -27,7 +27,8 @@ int main(int argc, char *argv[]){
   Eigen::VectorXd B(nV);
   // auto solf = [](Eigen::Vector3d v) { return (v[0]*v[0] - v[1]*v[1] + v[2] + sin(v[2])); }; // x^2 - y^2 + z
   // auto solf = [](Eigen::Vector3d v) { return sin(igl::PI * v[1]) * cos(igl::PI * v[0]);};
-  auto solf = [](Eigen::Vector3d v) { return log((Eigen::Vector3d(3.0,3.0,3.0)-v).norm()); }; // x^2 - y^2 + z
+  auto solf = [](Eigen::Vector3d v) { return 1/(Eigen::Vector3d(1.0,1.0,1.0)-v).norm(); }; 
+  auto solf_grad = [](Eigen::Vector3d v) { return (Eigen::Vector3d(1.0,1.0,1.0)-v)/pow((Eigen::Vector3d(1.0,1.0,1.0)-v).norm(),3); };
   for (int i = 0; i < nV; i++) {
     B[i] = solf(V.row(i));
   }
@@ -35,7 +36,7 @@ int main(int argc, char *argv[]){
 
   int w = 64, h = 64;
   int nquery = w * h;
-  Eigen::MatrixXd P(nquery, 3), U_grad(nquery, 3);
+  Eigen::MatrixXd P(nquery, 3), U_grad(nquery, 3), sol_grad(nquery,3);
   Eigen::VectorXd U(nquery), sol(nquery);
   // P << 0., 0., 0.;
   //     //  0.05,0.06,0.07,
@@ -54,7 +55,10 @@ int main(int argc, char *argv[]){
 
   for (int i = 0; i < nquery; i++) {
     sol[i] = solf(P.row(i));
+    sol_grad.row(i) = solf_grad(P.row(i));
   }
+
+  std::cout << "grad error: " << (sol_grad-U_grad).mean() << std::endl;
 
   auto write_solution = [=](const Eigen::VectorXd & u, const char* path){
     Eigen::Matrix<unsigned char,Eigen::Dynamic,Eigen::Dynamic> R(w,h);
